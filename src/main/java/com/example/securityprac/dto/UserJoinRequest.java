0a5e2d49b0260f0d553a5059f8238fdc4cc6ff53
JO YEJI <whdpwl2@gmail.com>
2022-11-29 13:11:31 +0900

    feat: 비밀번호 암호화해서 저장

--- src/main/java/com/example/securityprac/dto/UserJoinRequest.java
+++ src/main/java/com/example/securityprac/dto/UserJoinRequest.java
@@ -12,10 +12,10 @@ public class UserJoinRequest {
     private String userName;
     private String password;
     private String email;
-    public User toEntity(){
+    public User toEntity(String password){
         return User.builder()
                 .userName(this.userName)
-                .password(this.password)
+                .password(password)
                 .email(this.email)
                 .build();
     }
--- src/main/java/com/example/securityprac/service/UserService.java
+++ src/main/java/com/example/securityprac/service/UserService.java
@@ -7,12 +7,14 @@ import com.example.securityprac.exception.ErrorCode;
 import com.example.securityprac.exception.HospitalReviewAppException;
 import com.example.securityprac.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;    //비밀번호 인코더
 
     //비지니스 로직 - 회원 가입
     public UserDto join (UserJoinRequest request){
@@ -21,7 +23,7 @@ public class UserService {
                 .ifPresent(user -> {
                     throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s는 이미 존재하는 아이디입니다.", request.getUserName()));
                 });
-        User savedUser = userRepository.save(request.toEntity());
+        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
         return UserDto.builder()
                 .id(savedUser.getId())
                 .userName(savedUser.getUserName())
