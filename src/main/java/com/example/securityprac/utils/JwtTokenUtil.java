82bc7d3f322d86aa931486e3c179dafc86d8224f
JO YEJI <whdpwl2@gmail.com>
2022-12-05 14:30:26 +0900

    feat: 토큰 유효기간 확인 메서드 추가

--- src/main/java/com/example/securityprac/utils/JwtTokenUtil.java
+++ src/main/java/com/example/securityprac/utils/JwtTokenUtil.java
@@ -22,4 +22,13 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
+    //token의 Claim을 분리하는 메서드
+    public static Claims extractClaims(String token, String key){
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+    //token이 만료되었는지 체크
+    public static boolean isExpired(String token, String secretKey) {
+        Date expiredDate = extractClaims(token, secretKey).getExpiration(); //token의 유효기간 추출
+        return expiredDate.before(new Date());      //유효기간이 지금보다 전인지 확인
+    }
 }
